e2ae79ccd705a2531fa15a16c2add599ea18aae5
Josh Goodman <jogoodma@indiana.edu>
2016-08-27 02:11:55 -0400

    Added responsive container wrapper and a few sample BLAST result files.

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {render} from 'react-dom'
 
-import Component from '../../src'
+import AlignmentGraphic from '../../src'
+import jsonResult from './tblastx.json';
 
 let Demo = React.createClass({
   render() {
     return <div>
       <h1>react-alignment-graphic Demo</h1>
-      <Component/>
+      <AlignmentGraphic blastResult={jsonResult}/>
     </div>
   }
 })
