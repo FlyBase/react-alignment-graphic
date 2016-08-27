a76525277458e20e9879119fcf563df1a23d44c4
Josh Goodman <jogoodma@indiana.edu>
2016-08-27 15:01:00 -0400

    Added author to package.json.  Added fixed and responsive examples to demo page.

--- demo/src/index.js
+++ demo/src/index.js
@@ -5,12 +5,22 @@ import AlignmentGraphic from '../../src'
 import jsonResult from './tblastx.json';
 
 let Demo = React.createClass({
-  render() {
-    return <div>
-      <h1>react-alignment-graphic Demo</h1>
-      <AlignmentGraphic blastResult={jsonResult}/>
-    </div>
-  }
+    render() {
+        return (
+            <div>
+                <h1>react-alignment-graphic Demo</h1>
+                <h2>Fixed width</h2>
+                <div style={{width: '900px' }}>
+                    <AlignmentGraphic blastResult={jsonResult}/>
+                </div>
+                <h2>Responsive</h2>
+                <div>
+                    <h1>react-alignment-graphic Demo</h1>
+                    <AlignmentGraphic blastResult={jsonResult}/>
+                </div>
+            </div>
+        );
+    }
 })
 
 render(<Demo/>, document.querySelector('#demo'))
